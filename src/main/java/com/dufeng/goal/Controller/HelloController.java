d7fd1924fead6f6cef656310cc0fba9eaafbf4d0
yandufeng <yandufeng@readtv.cn>
2017-02-20 15:41:07 +0800

    close #5

--- src/main/java/com/dufeng/goal/Controller/HelloController.java
+++ src/main/java/com/dufeng/goal/Controller/HelloController.java
@@ -3,13 +3,13 @@ package com.dufeng.goal.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.dufeng.goal.domain.User;
 import com.dufeng.goal.service.HelloService;
 
-@RestController
+@Controller
 public class HelloController {
     
     @Autowired
@@ -20,6 +20,6 @@ public class HelloController {
         List<User> users = helloService.getUsers();
         System.out.println(users.size());
         
-        return "Hello, dufeng";
+        return "hello";
     }
 }
