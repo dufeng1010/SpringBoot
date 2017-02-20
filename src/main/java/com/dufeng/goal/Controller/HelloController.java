9b7dd95172251edebd5dee8f2cb3e103561b4428
yandufeng <yandufeng@readtv.cn>
2017-02-20 16:19:43 +0800

    close #9

--- src/main/java/com/dufeng/goal/Controller/HelloController.java
+++ src/main/java/com/dufeng/goal/Controller/HelloController.java
@@ -3,6 +3,7 @@ package com.dufeng.goal.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,8 +16,15 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
     
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    
     @RequestMapping("/")
     public String index() {
+        for(int i = 0; i < 5; i++) {
+            stringRedisTemplate.convertAndSend("chat", "hello fengfengzai!");
+        }
+        
         List<User> users = helloService.getUsers();
         System.out.println(users.size());
         
